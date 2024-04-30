facff0638bbb8b39aaa9d252dc3671bcf92f438c
shahadat-siam <lonelinessismycompanion09@gmail.com>
2024-04-30 18:28:00 +0600

    filter based customization

--- src/Component/MyList/MyList.jsx
+++ src/Component/MyList/MyList.jsx
@@ -6,14 +6,26 @@ import { FaAngleDown } from "react-icons/fa";
 const MyList = () => {
   const { user } = UseAuth();
   const [item, setItem] = useState();
+  const [displayItems, setDisplayItems] = useState([])
   const craft = item;
-  // console.log(craft)
+  console.log(displayItems)
+
+  const hundleItemFilter = filter => {
+    if(filter === 'Yes'){
+      const yes = item.filter(item => item.customization === 'yes')
+      setDisplayItems(yes)
+    } else if(filter === 'No'){
+      const no = item.filter(item => item.customization === 'no')
+      setDisplayItems(no)
+    }
+  }
 
   useEffect(() => {
     fetch(`https://y-three-wine.vercel.app/mylist/${user?.email}`)
       .then((res) => res.json())
       .then((data) => {
         setItem(data);
+        setDisplayItems(data)
         // console.log(data)
       });
   }, [user]);
@@ -31,17 +43,17 @@ const MyList = () => {
           </summary>
           <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-36 border border[#007F73]   ">
             <li className="flex items-center">
-              <a className="text-purple-500 text-center">Yes</a>
+              <a onClick={() => hundleItemFilter('Yes')} className="text-purple-500 text-center">Yes</a>
             </li>
             <li className="flex items-center">
-              <a className="text-purple-500 text-center">No</a>
+              <a onClick={() => hundleItemFilter('No')} className="text-purple-500 text-center">No</a>
             </li>
           </ul>
         </details>
       </div>
 
       <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-3 p-3 lg:px-8">
-        {item?.map((itm) => (
+        {displayItems?.map((itm) => (
           <MylistCard key={itm._id} item={itm} craft={craft}></MylistCard>
         ))}
       </div>
